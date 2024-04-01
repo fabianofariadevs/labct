a1acc15f84627df3e4b633811b07f0257ae7a492
fabianofariadevs <fabianofariadevs2022@gmail.com>
2024-04-01 13:40:01 -0300

    dep3

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.append('/Users/fabia/VsCodeProjects/labct24/labct')
+sys.path.append('/Users/fabia/VsCodeProjects/labct24')
 
 wsgi_app = 'labct.app:app'
-bind = '0.0.0.0:8080'
+bind = '0.0.0.0:5000'
 workers = 4
