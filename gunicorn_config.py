0387a2c0488331329bfd0f4d48cf8db033f8f151
fabianofariadevs <fabianofariadevs2022@gmail.com>
2024-04-01 11:18:04 -0300

    deploy

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.append('/home/ubuntu/labct24')
+sys.path.append('/home/ubuntu/labct24/labct')
 
 wsgi_app = 'labct.app:app'
-bind = '0.0.0.0:5000'
+bind = '0.0.0.0:8080'
 workers = 4
